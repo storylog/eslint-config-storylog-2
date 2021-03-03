c7b5a0ba2cdd4e8505029367594c61a857e9e767
chutiphon-k <chutiphon.ksp@gmail.com>
2021-03-03 17:55:11 +0700

    v1.0.3

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 module.exports = require('./.eslintrc.js');
-
-const x = {};
-console.log('------------------------------------');
-console.log(x.a._id);
-console.log('------------------------------------');
